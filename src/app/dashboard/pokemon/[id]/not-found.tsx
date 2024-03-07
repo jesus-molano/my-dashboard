657ee9e5a49d77af8835767eab7fa94259c92c58
jesus-molano <jessumolano@gmail.com>
2024-03-07 16:00:19 +0000

    Added not founds

--- src/app/dashboard/pokemon/[id]/not-found.tsx
+++ src/app/dashboard/pokemon/[id]/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFoundPage() {
           <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
           <span className="relative block px-8 py-3 bg-[#1A2238] border border-current">
-            <Link href="/dashboard/pokemons">Go Home</Link>
+            <Link href="/dashboard/pokemons">See All Pokemons</Link>
           </span>
         </div>
       </button>
